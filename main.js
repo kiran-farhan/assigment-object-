38c71c867dddd4954181c189b4558f761098d00a
kiran-farhan <159946236+kiran-farhan@users.noreply.github.com>
2024-03-15 22:21:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// my third assigment (object)
+var guestlist = {
+    vipguest: ["ayesha", "mahnoor", "ateefa", "sareema"],
+    localguest: ["dilshad", "hina", "famina", "tina"],
+    boy1: "ali",
+    boy2: "farhan",
+    boy3: "ammar",
+    adress: {
+        venue: "governorhouse",
+        city: "karachi",
+    }
+};
+console.log(guestlist.adress.venue);
+console.log(guestlist.boy3);
+console.log(guestlist.localguest);
---
+++ main.ts
@@ -0,0 +1,19 @@
+// my third assigment (object)
+let guestlist={
+    vipguest:[ "ayesha","mahnoor","ateefa","sareema"],
+    localguest: [ "dilshad","hina","famina","tina" ],    
+boy1:"ali",
+boy2: "farhan",
+boy3:"ammar",
+adress:{
+    venue: "governorhouse",
+    city:"karachi",
+}
+}
+
+console.log(guestlist.adress.venue);
+console.log(guestlist.boy3);
+console.log(guestlist.localguest)
+
+
+
